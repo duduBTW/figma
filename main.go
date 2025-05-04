521933567f9d217229d9d9f1086a2ea09b4db158
Arthur Felipe <tutuzindaquebrada@gmail.com>
2025-05-03 21:36:15 -0700

    I added the variables for the retangle

--- main.go
+++ main.go
@@ -10,10 +10,16 @@ func main() {
 
 	rl.SetTargetFPS(60)
 
+	var posX int32 = 0
+	var posY int32 = 0
+	var width int32 = 0
+	var height int32 = 0
+	var col = rl.Black
+
 	for !rl.WindowShouldClose() {
 		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Hello!", 190, 200, 20, rl.LightGray)
+		rl.DrawRectangle(posX, posY, width, height, col)
 		rl.EndDrawing()
 	}
 }
